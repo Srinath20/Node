ec55bbb62eabc4ec2f72dee4a2f253de9dd68976
Srinath20 <srinathgollapinni>
2024-02-16 18:22:01 +0530

    Writing and Reading a file

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-let htp = require('http');//We require http module to create a server in nodejs
+const htp = require('http');//We require http module to create a server in nodejs
+const fs = require('fs');
 htp.createServer((req,res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
       res.write('<html>');
       res.write('<head><title>Enter a message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="mess"><button type="submit">Send</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="name"><button type="submit">Send</button></form></body>');
     res.write('</html');
     return res.end();
+    }
+    if(url ==='/message' && method ==='POST'){
+      const body = [];
+      req.on('data', (chunks) => {
+        body.push(chunks);
+      });
+      req.on('end',() =>{
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        //fs.writeFileSync() methods blocks the code bwlo it from getting executed which might be a problem in reading/copying big files. So we prefer to use fs.writeFile();
+        fs.writeFile('names.txt',message,(err) =>{
+          res.statusCode = '302';
+        res.setHeader('Location','/');
+      return res.end(); 
+        }); 
+      })
     }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<body><h1>Welcome</h1></body>');
     res.write('</html');
-  //  res.write("Welcome Back folks!!:)");
-  //  res.end(); process.exit(); // or you can write res.end("Welcome back folks")
-}).listen(3000); //http://localhost:3000/
-
-
-
-/*
-HTTP:This module allows transfer of data between node.js and computer throught hyper text transfer protocol.
-
-   FS: File system allows node.js to interact with file systems on the computer its running.
-
-	PATH : It allows to manipulate with the files.
-
-	EVENTS : It helps in creating custome events and event listeners in node.js
-
-
-*/
+  }).listen(3000);

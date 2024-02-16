df001ae2295c486ec7bfcc003bc5223b01c8ec53
Srinath20 <srinathgollapinni>
2024-02-16 20:12:42 +0530

    Clean Up Code

---
+++ routes.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const requestHandler = (req,res) =>{
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter a message</title><head>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="name"><button type="submit">Send</button></form></body>');
+      res.write('</html');
+      return res.end();
+      }
+      if(url ==='/message' && method ==='POST'){
+        const body = [];
+        req.on('data', (chunks) => {
+          body.push(chunks);
+        });
+        req.on('end',() =>{
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split('=')[1];
+          //fs.writeFileSync() methods blocks the code bwlo it from getting executed which might be a problem in reading/copying big files. So we prefer to use fs.writeFile();
+          fs.writeFile('names.txt',message,(err) =>{
+            res.statusCode = '302';
+          res.setHeader('Location','/');
+        return res.end(); 
+          }); 
+        })
+      }
+      res.setHeader('Content-Type','text/html');
+      res.write('<html>');
+      res.write('<body><h1>Welcome</h1></body>');
+      res.write('</html');
+      res.end();
+}
+
+module.exports = requestHandler;

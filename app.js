78313b0771e8be65ee9870c649378a7944cdbc21
Srinath20 <srinathgollapinni>
2024-02-23 11:41:55 +0530

    First commit on Express.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const htp = require('http');//We require http module to create a server in nodejs
-const route = require('./routes');
-htp.createServer((route)).listen(3000);
+const express = require('express');
+const app = express();
+app.use((req,res,next) =>{
+    console.log("In middle Ware");
+    next(); // You need to call next() to pass control to the next middleware
+})
+app.use((req,res,next) =>{
+    console.log("In second middle ware");
+    res.setHeader('Content-Type', 'text/html');
+    res.send('<h1>Hello From Express JS</h1>'); // When we are sending response, we are not looking for next middle ware function and sending response to end user.
+
+})
+app.listen(4000);

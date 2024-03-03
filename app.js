db6bb013413d6554d1df35cd3081fe4817fe9bde
Srinath20 <srinathgollapinni>
2024-03-03 09:44:33 +0530

    EJS part01

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminroutes = require('./routes/admin');
-const shoproutes = require('./routes/shop');
-const path = require('path');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')))//Registering a static folder
-app.use(bodyParser.urlencoded({ extended: false }));/*
-setting up middleware for the application to parse incoming request bodies that have a URL-encoded format. 
-The extended: false option means that only primitive data types (like strings, numbers, and booleans) will be parsed. 
-Complex data types, such as objects and arrays, will not be parsed.
-*/
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/admin', adminroutes);
-app.use('/', shoproutes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// Catch-all route for handling undefined routes
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
--- controllers/shopController.js
+++
@@ -1,7 +0,0 @@
-// shopController.js
-const path = require('path');
-
-exports.getIndex = (req, res, next) => {
-    console.log("In Main middleware");
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
-};
